a7b5e0768e9ae2562bab1317a0dc6451cda4cc73
Willi Schinmeyer <willi@schinmeyer.de>
2025-01-29 20:41:38 +0100

    add file pickers

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
-function greet(name: string) {
-    console.log(`Hello, ${name}!`);
+const targetGLAButton = document.getElementById('targetGLAFilePickerButton') as HTMLInputElement;
+const targetConfigButton = document.getElementById('targetConfigFilePickerButton') as HTMLInputElement;
+const targetConfigWarning = document.getElementById('targetConfigFileWarning') as HTMLSpanElement;
+const sourceGLAButton = document.getElementById('sourceGLAFilePickerButton') as HTMLInputElement;
+const sourceConfigButton = document.getElementById('sourceConfigFilePickerButton') as HTMLInputElement;
+const sourceConfigWarning = document.getElementById('sourceConfigFileWarning') as HTMLSpanElement;
+const loadButton = document.getElementById('loadButton') as HTMLButtonElement;
+
+// the "files is FileList" type guard assures typescript that files is not null if the return value is true
+const filesAvailable = (files: FileList | null): files is FileList => files !== null && files.length > 0;
+
+const fileChanged = () => {
+    loadButton.disabled = !(filesAvailable(targetGLAButton.files) && filesAvailable(targetConfigButton.files) && filesAvailable(sourceGLAButton.files) && filesAvailable(sourceConfigButton.files));
+    targetConfigWarning.innerHTML = (filesAvailable(targetConfigButton.files) && targetConfigButton.files[0].name !== 'animation.cfg') ? 'Warning: expected file called "animation.cfg"' : '';
+    sourceConfigWarning.innerHTML = (filesAvailable(sourceConfigButton.files) && sourceConfigButton.files[0].name !== 'animation.cfg') ? 'Warning: expected file called "animation.cfg"' : '';
 }
 
-greet("World");
-
-// Example of a DOM manipulation
-const body = document.querySelector('body');
-if (body) {
-    body.innerHTML += "<p>This is added by TypeScript!</p>";
+const load = () => {
+    console.log(`todo: load ${targetGLAButton.files} ${targetConfigButton.files} ${sourceGLAButton.value} ${sourceConfigButton.value}`);
 }
